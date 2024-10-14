37bfe8b7cdf7cf6b6b7e4a9909785b58b40f7b28
laneb14 <laneb14@alumni.hanover.edu>
2024-10-14 13:53:19 -0500

    Update LitterDecompositionMicrobialCommunities.R
    Update based on comments in peer review

--- LitterDecompositionMicrobialCommunities.R
+++ LitterDecompositionMicrobialCommunities.R
@@ -9,6 +9,9 @@ library(expss)
 library(pairwiseAdonis)
 library(ape)
 library(ggplot2)
+library(ggResidpanel)
+
+# Note: Throughout I use Shannon's Evenness, or "SEven" in place of Pielou's. These metrics are the same, but named differently.
 
 # Set working directory as needed
 setwd("C:/Users/brett/Dropbox/Decomposition Experiment/FinalCodeAndData/UsingDataFromEDI")
@@ -50,24 +53,26 @@ EvenObject <- transform_sample_counts(PhyloObject,function(x) (x/sum(x))*100 )
 # Adequate sequencing depth confirmed through manual inspection of rarefaction plots formed from original data (not transformed)
 # Viewing them in sets of 25 to facilitate inspection
 # All curves should approach they asysmptote
-rarecurve(t(otu_table(PhyloObject))[1:25,], step=1000, cex=0.5)
-rarecurve(t(otu_table(PhyloObject))[26:50,], step=1000, cex=0.5)
-rarecurve(t(otu_table(PhyloObject))[51:75,], step=1000, cex=0.5)
-rarecurve(t(otu_table(PhyloObject))[76:100,], step=1000, cex=0.5)
-rarecurve(t(otu_table(PhyloObject))[101:125,], step=1000, cex=0.5)
-rarecurve(t(otu_table(PhyloObject))[126:150,], step=1000, cex=0.5)
-rarecurve(t(otu_table(PhyloObject))[151:175,], step=1000, cex=0.5)
-rarecurve(t(otu_table(PhyloObject))[176:200,], step=1000, cex=0.5)
-rarecurve(t(otu_table(PhyloObject))[201:225,], step=1000, cex=0.5)
-rarecurve(t(otu_table(PhyloObject))[226:250,], step=1000, cex=0.5)
-rarecurve(t(otu_table(PhyloObject))[251:275,], step=1000, cex=0.5)
-rarecurve(t(otu_table(PhyloObject))[276:300,], step=1000, cex=0.5)
-rarecurve(t(otu_table(PhyloObject))[301:325,], step=1000, cex=0.5)
-rarecurve(t(otu_table(PhyloObject))[326:350,], step=1000, cex=0.5)
-rarecurve(t(otu_table(PhyloObject))[351:368,], step=1000, cex=0.5)
-
+if(1==2){ # These take a while to run and only need to be viewed once. Add this step so they have to be viewed manually
+  rarecurve(t(otu_table(PhyloObject))[1:25,], step=1000, cex=0.5)
+  rarecurve(t(otu_table(PhyloObject))[26:50,], step=1000, cex=0.5)
+  rarecurve(t(otu_table(PhyloObject))[51:75,], step=1000, cex=0.5)
+  rarecurve(t(otu_table(PhyloObject))[76:100,], step=1000, cex=0.5)
+  rarecurve(t(otu_table(PhyloObject))[101:125,], step=1000, cex=0.5)
+  rarecurve(t(otu_table(PhyloObject))[126:150,], step=1000, cex=0.5)
+  rarecurve(t(otu_table(PhyloObject))[151:175,], step=1000, cex=0.5)
+  rarecurve(t(otu_table(PhyloObject))[176:200,], step=1000, cex=0.5)
+  rarecurve(t(otu_table(PhyloObject))[201:225,], step=1000, cex=0.5)
+  rarecurve(t(otu_table(PhyloObject))[226:250,], step=1000, cex=0.5)
+  rarecurve(t(otu_table(PhyloObject))[251:275,], step=1000, cex=0.5)
+  rarecurve(t(otu_table(PhyloObject))[276:300,], step=1000, cex=0.5)
+  rarecurve(t(otu_table(PhyloObject))[301:325,], step=1000, cex=0.5)
+  rarecurve(t(otu_table(PhyloObject))[326:350,], step=1000, cex=0.5)
+  rarecurve(t(otu_table(PhyloObject))[351:368,], step=1000, cex=0.5)
+}
 ##### Calculate alpha diversity #####
 # Alpha diversity metrics are calculated using data rarified to an equal sampling depth
+set.seed(55735)
 RarePhylo <- rarefy_even_depth(PhyloObject)
 Indicies <- estimate_richness(RarePhylo,measures=c('Observed','Chao1','Shannon')) # Calculate Chao1 and Shannon's Diversity
 rownames(Indicies) <- gsub("\\.","-",rownames(Indicies)) # I'm not sure why it replaces dashes with periods, but this fixes it
@@ -76,122 +81,160 @@ Indicies$Infection <- vlookup(rownames(Indicies),InputMeta,'Infection','SampleNa
 Indicies$Month <- vlookup(rownames(Indicies),InputMeta,'Month','SampleName')
 Indicies$Replicate <- vlookup(rownames(Indicies),InputMeta,'Replicate','SampleName')
 
-##### Table S6 #####
+##### Table S4, right three columns ####
 
-head(Indicies)
+for (z in 1:3){
+    if(z==1){print('Status,Month,Chao,Shannon,Pielou')}
+  for (m in c('Start','February','April','June','August','October','December')){
+    if(z==1)(TempDF <- Indicies[Indicies$Month==m & Indicies$Infection=='Infected',])
+    if(z==2)(TempDF <- Indicies[Indicies$Month==m & Indicies$Infection=='Non-infected',])
+    if(z==3)(TempDF <- Indicies[Indicies$Month==m & Indicies$Infection=='Common Garden',])
+    if(nrow(TempDF)==0){stop('no rows found')}
+    a <- round(mean(TempDF$Chao1),2)
+    b <- round(sd(TempDF$Chao1)/sqrt(length(TempDF$Chao1)),2)
+    chao1 <- paste(sep='',a,' (',b,')')
+    a <- round(mean(TempDF$Shannon),2)
+    b <- round(sd(TempDF$Shannon)/sqrt(length(TempDF$Shannon)),3)
+    Shan <- paste(sep='',a,' (',b,')')
+    a <- round(mean(TempDF$SEven),2)
+    b <- round(sd(TempDF$SEven)/sqrt(length(TempDF$SEven)),3)
+    Piel <- paste(sep='',a,' (',b,')')
+    print(paste(TempDF$Infection[1],TempDF$Month[1],chao1,Shan,Piel,sep=','))
+    rm(a,b,chao1,Shan,Piel,m)
+  }
+  rm(z)
+}
+
+##### Table S5 #####
+
+## Compare Infected and Non Infected
+
+hist(Indicies$Chao1[Indicies$Infection!='Common Garden'])
+shapiro.test(Indicies$Chao1[Indicies$Infection!='Common Garden'])
+hist(sqrt(Indicies$Chao1[Indicies$Infection!='Common Garden']))
+shapiro.test(sqrt(Indicies$Chao1[Indicies$Infection!='Common Garden']))
+Chao1.NI.I <- lmer(sqrt(Chao1)~Infection*Month+(1|Replicate),Indicies[Indicies$Infection!='Common Garden',])
+resid_panel(Chao1.NI.I)
+anova(Chao1.NI.I)
+
+
+hist(Indicies$Shannon[Indicies$Infection!='Common Garden'])
+shapiro.test(Indicies$Shannon[Indicies$Infection!='Common Garden'])
+hist((Indicies$Shannon[Indicies$Infection!='Common Garden'])^(3))
+shapiro.test((Indicies$Shannon[Indicies$Infection!='Common Garden'])^(3))
+Shannon.NI.I <- lmer((Shannon)^3~Infection*Month+(1|Replicate),Indicies[Indicies$Infection!='Common Garden',])
+resid_panel(Shannon.NI.I)
+anova(Shannon.NI.I)
+
+hist(Indicies$SEven[Indicies$Infection!='Common Garden'])
+shapiro.test(Indicies$SEven[Indicies$Infection!='Common Garden'])
+hist((Indicies$SEven[Indicies$Infection!='Common Garden'])^3)
+shapiro.test((Indicies$SEven[Indicies$Infection!='Common Garden'])^3)
+Pielou.NI.I<- lmer((SEven)^3~Infection*Month+(1|Replicate),Indicies[Indicies$Infection!='Common Garden',])
+resid_panel(Pielou.NI.I)
+anova(Pielou.NI.I)
+
+## Same as above but compare Common Garden with Non-Infected
+
+CG.NI.DF <- Indicies[Indicies$Infection!='Infected',]
+
+hist(CG.NI.DF$Chao1)
+shapiro.test(CG.NI.DF$Chao1)
+Chao1.CG <- lm(Chao1~Infection*Month, CG.NI.DF)
+resid_panel(Chao1.CG)
+anova(Chao1.CG)
+
+
+hist(CG.NI.DF$Shannon)
+shapiro.test(CG.NI.DF$Shannon)
+hist(CG.NI.DF$Shannon^2)
+shapiro.test(CG.NI.DF$Shannon^2)
+CG.NI.DF$Shannon.sq <- CG.NI.DF$Shannon^2
+Shannon.CG <- lm(Shannon.sq~Infection*Month, CG.NI.DF)
+resid_panel(Shannon.CG)
+anova(Shannon.CG)
+
+
+hist(CG.NI.DF$SEven)
+shapiro.test(CG.NI.DF$SEven)
+hist(CG.NI.DF$SEven^2)
+shapiro.test(CG.NI.DF$SEven^2)
+CG.NI.DF$SEven.sq <- CG.NI.DF$SEven^2
+Pielous.CG <- lm(SEven.sq~Infection*Month, CG.NI.DF)
+resid_panel(Pielous.CG)
+anova(Pielous.CG)
+
+##### Table S6, Pairwise permanova #####
 
 for (x in MonthOrder){
-  MonthDF <- Indicies[Indicies$Month==x,]
-  # break
-  if (x==MonthOrder[1]){
-    #Initiate dataframe
-    TableS6 <- data.frame(Metric=vector(),Month=vector(),CG=numeric(),NI=numeric(),I=numeric(),CG.NI.p=numeric(),CG.NI.q=numeric(),NI.I.p=numeric(),NI.I.q=numeric(),CG.NI.f=numeric(),NI.I.f=numeric() )
-  }
-  ## Chao1 ##
-  CG.NI <- anova(lm(Chao1~Infection,MonthDF[MonthDF$Infection!='Infected',]))
-  NI.I <- anova(lm(Chao1~Infection,MonthDF[MonthDF$Infection!='Common Garden',]))
-  CG.NI.q <- p.adjust(CG.NI$`Pr(>F)`[1],'bonferroni',n=7)
-  NI.I.q <- p.adjust(NI.I$`Pr(>F)`[1],'bonferroni',n=7)
-  TableS6 <- rbind(TableS6,
-  data.frame(Metric='Chao1',Month=x,CG=round(mean(MonthDF$Chao1[MonthDF$Infection=='Common Garden']),digits=2),
-             NI=round(mean(MonthDF$Chao1[MonthDF$Infection=='Non-infected']),digits=2),I=round(mean(MonthDF$Chao1[MonthDF$Infection=='Infected']),digits=2),
-             CG.NI.p=round(CG.NI$`Pr(>F)`[1],3),CG.NI.q=round(CG.NI.q,3),NI.I.p=round(NI.I$`Pr(>F)`[1],3), NI.I.q=round(NI.I.q,3),
-             CG.NI.f=CG.NI$`F value`[1]%>%round(3),NI.I.f=NI.I$`F value`[1]%>%round(3) )
-  )
-  ## Shannon Diversity ##
-  CG.NI <- anova(lm(Shannon~Infection,MonthDF[MonthDF$Infection!='Infected',]))
-  NI.I <- anova(lm(Shannon~Infection,MonthDF[MonthDF$Infection!='Common Garden',]))
-  CG.NI.q <- p.adjust(CG.NI$`Pr(>F)`[1],'bonferroni',n=7)
-  NI.I.q <- p.adjust(NI.I$`Pr(>F)`[1],'bonferroni',n=7)
-  TableS6 <- rbind(TableS6,
-  data.frame(Metric='Shannon Diversity',Month=x,CG=round(mean(MonthDF$Shannon[MonthDF$Infection=='Common Garden']),digits=2),
-             NI=round(mean(MonthDF$Shannon[MonthDF$Infection=='Non-infected']),digits=2),I=round(mean(MonthDF$Shannon[MonthDF$Infection=='Infected']),digits=2),
-             CG.NI.p=round(CG.NI$`Pr(>F)`[1],3),CG.NI.q=round(CG.NI.q,3),NI.I.p=round(NI.I$`Pr(>F)`[1],3), NI.I.q=round(NI.I.q,3),
-             CG.NI.f=CG.NI$`F value`[1]%>%round(3),NI.I.f=NI.I$`F value`[1]%>%round(3) )
-  )
-  ## Shannon Evenness ##
-  CG.NI <- anova(lm(SEven~Infection,MonthDF[MonthDF$Infection!='Infected',]))
-  NI.I <- anova(lm(SEven~Infection,MonthDF[MonthDF$Infection!='Common Garden',]))
-  CG.NI.q <- p.adjust(CG.NI$`Pr(>F)`[1],'bonferroni',n=7)
-  NI.I.q <- p.adjust(NI.I$`Pr(>F)`[1],'bonferroni',n=7)
-  TableS6 <- rbind(TableS6,
-  data.frame(Metric='Shannon Evenness',Month=x,CG=round(mean(MonthDF$SEven[MonthDF$Infection=='Common Garden']),digits=2),
-             NI=round(mean(MonthDF$SEven[MonthDF$Infection=='Non-infected']),digits=2),I=round(mean(MonthDF$SEven[MonthDF$Infection=='Infected']),digits=2),
-             CG.NI.p=round(CG.NI$`Pr(>F)`[1],3),CG.NI.q=round(CG.NI.q,3),NI.I.p=round(NI.I$`Pr(>F)`[1],3), NI.I.q=round(NI.I.q,3),
-             CG.NI.f=CG.NI$`F value`[1]%>%round(3),NI.I.f=NI.I$`F value`[1]%>%round(3) )
-  )
-  
-  rm(x,CG.NI.q,NI.I.q,CG.NI,NI.I,MonthDF)
+  head(sample_data(EvenObject))
+  TempObject <- subset_samples(EvenObject, Infection!='Common Garden' & Month==x)
+  TempDist <- as(vegdist(t(otu_table(TempObject)),method='bray'),'matrix')
+  TempDF <- as(sample_data(TempObject),'data.frame')
+  Res <- adonis2(TempDist~Infection,data=TempDF,permutations = 10000)
+  y <- Res$`Pr(>F)`[1]
+  y2 <- ifelse(y<=0.001,"p<0.001",y)
+  z <- ifelse(y<=0.001,"p<0.001",p.adjust(y,'bonferroni',n=7) )
+  print(paste(sep=',','Non vs Infected','Bray-Curtis',x,Res$F[1],y2,z))
+  rm(TempDist,y,y2,z,Res)
+  TempDist <- as(vegdist(t(otu_table(TempObject)),method='jaccard'),'matrix')
+  Res <- adonis2(TempDist~Infection,data=TempDF,permutations = 10000)
+  y <- Res$`Pr(>F)`[1]
+  y2 <- ifelse(y<=0.001,"p<0.001",y)
+  z <- ifelse(y<=0.001,"p<0.001",p.adjust(y,'bonferroni',n=7) )
+  print(paste(sep=',','Non vs Infected','Jaccard',x,Res$F[1],y2,z))
+  rm(x,y,y2,z,Res,TempDist,TempDF,TempObject)
+  # Can copy and paste the results into excel for manipulation, comma delimited
 }
 
-TableS6[TableS6$Metric=='Chao1',]             %>% select(Metric,Month,CG.NI.f,CG.NI.p)
-TableS6[TableS6$Metric=='Shannon Diversity',] %>% select(Metric,Month,CG.NI.f,CG.NI.p)
-TableS6[TableS6$Metric=='Shannon Evenness',]  %>% select(Metric,Month,CG.NI.f,CG.NI.p)
-
-TableS6[TableS6$Metric=='Chao1',]             %>% select(Metric,Month,NI.I.f,NI.I.p,NI.I.q)
-TableS6[TableS6$Metric=='Shannon Diversity',] %>% select(Metric,Month,NI.I.f,NI.I.p,NI.I.q)
-TableS6[TableS6$Metric=='Shannon Evenness',]  %>% select(Metric,Month,NI.I.f,NI.I.p,NI.I.q)
-
-##### Table S7 #####
-# Need to do pairwise adonis for both Bray+Jaccard by Healthy+Infected
-
-for (I in c('Non-infected','Infected')){
-  if (I == 'Non-infected'){ # on first iteration only
-    TableS7 <- data.frame(Infection=vector(),Distance=vector(),Months=vector(),qval=numeric())
-  }
-  for ( D in c('bray','jaccard')){
-    # break
-    ps <- prune_samples(sample_data(EvenObject)$Infection==I,EvenObject) # subset samples by infection, iteration of phyloseq used here has a problem using variables in subset_samples
-    ps <- prune_taxa(taxa_sums(ps)>0,ps)
-    dist <- t(otu_table(ps)) %>% vegdist(method=D)
-    # month <- sample_data(ps)$Month
-    df <- sample_data(ps) %>% as('data.frame')
-    result <- pairwise.adonis(x=dist,factors=df$Month,sim.method=D,perm=10000)
-    result2 <- result %>% select(Months=pairs,qval=p.adjusted)
-    result2$Infection <- I
-    result2$Distance <- D
-    TableS7<-rbind(TableS7,result2)
-    rm(ps,dist,df,result,result2)
-  }
-  rm(D,I)
+for (x in MonthOrder){
+  head(sample_data(EvenObject))
+  TempObject <- subset_samples(EvenObject, Infection!='Infected' & Month==x)
+  TempDist <- as(vegdist(t(otu_table(TempObject)),method='bray'),'matrix')
+  TempDF <- as(sample_data(TempObject),'data.frame')
+  Res <- adonis2(TempDist~Infection,data=TempDF,permutations = 10000)
+  y <- Res$`Pr(>F)`[1]
+  y2 <- ifelse(y<=0.001,"p<0.001",y)
+  z <- ifelse(y<=0.001,"p<0.001",p.adjust(y,'bonferroni',n=7) )
+  print(paste(sep=',','Common Garden vs Non','Bray-Curtis',x,Res$F[1],y2,z))
+  rm(TempDist,y,y2,z,Res)
+  TempDist <- as(vegdist(t(otu_table(TempObject)),method='jaccard'),'matrix')
+  Res <- adonis2(TempDist~Infection,data=TempDF,permutations = 10000)
+  y <- Res$`Pr(>F)`[1]
+  y2 <- ifelse(y<=0.001,"p<0.001",y)
+  z <- ifelse(y<=0.001,"p<0.001",p.adjust(y,'bonferroni',n=7) )
+  print(paste(sep=',','Common Garden vs Non','Jaccard',x,Res$F[1],y2,z))
+  rm(x,y,y2,z,Res,TempDist,TempDF,TempObject)
+  # Can copy and paste the results into excel for manipulation, comma delimited
 }
 
-head(TableS7)
+##### Table S7, Pairwise permanova #####
 
-##### Table S8 #####
+TempObject <- subset_samples(EvenObject, Infection=='Infected')
+TempDist <- as(vegdist(t(otu_table(TempObject)),method='bray'),'matrix')
+TempDF <- as(sample_data(TempObject),'data.frame')
+pairwise.adonis2(TempDist~Month,TempDF,permutations=10000)
+rm(TempObject,TempDist,TempDF)
 
-  # if (I == 'Non-infected'){ # on first iteration only
-    # TableS7 <- data.frame(Infection=vector(),Distance=vector(),Months=vector(),qval=numeric())
-  # }
-for ( D in c('bray','jaccard')){
-  if (D == "bray"){ # on first iteration only
-    TableS8 <- data.frame(Distance=vector(),Month=vector(),CG.NI.p=numeric(),CG.NI.q=numeric(),NI.I.p=numeric(),NI.I.q=numeric() )
-  }
-  for ( x in MonthOrder){
-    ## First subset the selected month
-    ps <- prune_samples(sample_data(EvenObject)$Month==x,EvenObject) # subset samples by Month, iteration of phyloseq used here has a problem using variables in subset_samples
-    ## Common garden vs non-infected
-    NoI <- subset_samples(ps,Infection!='Infected')
-    NoI <- prune_taxa(taxa_sums(NoI)>0,NoI)
-    dist.NoI <-t(otu_table(NoI)) %>% vegdist(method=D)
-    df.NoI <- sample_data(NoI) %>% as("data.frame")
-    NoI.result <- adonis2(dist.NoI~Infection,data=df.NoI,permutations=10000)
-    NoI.q <- NoI.result$`Pr(>F)`[1] %>% p.adjust(method='bonferroni',n=7)
-    
-    ## Non-infected vs infected
-    NoCG <- subset_samples(ps,Infection!='Common Garden')
-    NoCG <- prune_taxa(taxa_sums(NoCG)>0,NoCG)
-    dist.NoCG <-t(otu_table(NoCG)) %>% vegdist(method=D)
-    df.NoCG <- sample_data(NoCG) %>% as("data.frame")
-    NoCG.result <- adonis2(dist.NoCG~Infection,data=df.NoCG,permutations=10000)
-    NoCG.q <- NoCG.result$`Pr(>F)`[1] %>% p.adjust(method='bonferroni',n=7)
-    
-    TableS8<- rbind(TableS8,data.frame(Distance=D,Month=x, CG.NI.p=NoI.result$`Pr(>F)`[1],CG.NI.q=NoI.q,NI.I.p=NoCG.result$`Pr(>F)`[1],NI.I.q=NoCG.q ) )
-    rm(ps,NoI,NoCG,NoCG.result,NoI.result,NoI.q,NoCG.q,dist.NoCG,dist.NoI,df.NoCG,df.NoI,D,x)
-  }
-}
+TempObject <- subset_samples(EvenObject, Infection=='Non-infected')
+TempDist <- as(vegdist(t(otu_table(TempObject)),method='bray'),'matrix')
+TempDF <- as(sample_data(TempObject),'data.frame')
+pairwise.adonis2(TempDist~Month,TempDF,permutations=10000)
+rm(TempObject,TempDist,TempDF)
 
-##### Table S9 #####
+TempObject <- subset_samples(EvenObject, Infection=='Infected')
+TempDist <- as(vegdist(t(otu_table(TempObject)),method='jaccard'),'matrix')
+TempDF <- as(sample_data(TempObject),'data.frame')
+pairwise.adonis2(TempDist~Month,TempDF,permutations=10000)
+rm(TempObject,TempDist,TempDF)
+
+TempObject <- subset_samples(EvenObject, Infection=='Non-infected')
+TempDist <- as(vegdist(t(otu_table(TempObject)),method='jaccard'),'matrix')
+TempDF <- as(sample_data(TempObject),'data.frame')
+pairwise.adonis2(TempDist~Month,TempDF,permutations=10000)
+rm(TempObject,TempDist,TempDF)
+
+##### Table S9, Beta Disper #####
 
 for (I in c('Non-infected','Infected')){
   if (I == 'Non-infected'){ # on first iteration only
@@ -219,49 +262,3 @@ for (I in c('Non-infected','Infected')){
 }
 
 TableS9
-TableS9[TableS9$Infection=='Non-infected',]
-TableS9[TableS9$Infection=='Infected',]
-
-##### Figure 5 Main ordination #####
-
-EvenObject.NoCG <- subset_samples(EvenObject, Infection!='Common Garden')
-data <- sample_data(EvenObject.NoCG) %>% as("data.frame")
-BrayDist <- vegdist(t(otu_table(EvenObject.NoCG)), method='bray')
-res <- pcoa(BrayDist)
-vectors <- res$vectors %>% as.data.frame()
-vectors$Month <- vlookup(row.names(vectors),data,'Month','SampleName')
-vectors$Infection <- vlookup(row.names(vectors),data,'Infection','SampleName')
-# vectors$LitterSource <- vlookup(row.names(vectors),data,'Litter.Source','SampleName')
-vectors$Replicate <- vlookup(row.names(vectors),data,'Replicate','SampleName')
-OrdNoCG <- vectors
-
-res$values$Relative_eig[1]*100 # Copy these values into the axis labels below
-res$values$Relative_eig[2]*100 # Copy these values into the axis labels below
-
-ggplot(OrdNoCG,aes(x=Axis.1*(-1),y=Axis.2,color=factor(Month,levels=MonthOrder), shape=Infection))+
-  scale_color_discrete(name='Month')+ theme_bw()+  geom_point()+  stat_ellipse(aes(group=Month))+
-  theme(panel.grid = element_blank())+
-  scale_shape_manual(values = c('Non-infected'='circle',"Infected"='triangle'),labels=Lvalues,name="Litter Source")+
-  ylab("Axis 1  (15.32% Variation)")+xlab("Axis 2  (6.01% Variation)")
-
-##### Figure 6 ######
-
-# These values originated from above
-Facet67SigDF <- data.frame(Month=c('Start','February','April','June','August','October','December'),
-                           Axis.6=rep(0.23,n=7),Axis.7=rep(-0.3,n=7),
-                           # pval=c('p<0.001','p<0.001','p<0.001','p=0.126','p=0.049','p=0.351','p=0.819')  )
-                           pval=c('q<0.001','q<0.001','q<0.001','q=0.126','q=0.049','q=0.351','q=0.819')  )
-
-ggplot(OrdNoCG,aes(x=Axis.6,y=Axis.7,color=Infection))+theme_bw()+geom_point()+
-  stat_ellipse()+xlab("Axis 6  (2.25% Variation)")+ylab("Axis 7  (2.11% Variation)")+
-  theme(panel.grid = element_blank() )+
-  facet_wrap(~factor(Month,levels = c('Start','February','April','June','August','October','December')), ncol=2)+
-  geom_text(data=Facet67SigDF,inherit.aes=F,
-            aes(x=Axis.6,y=Axis.7,label=pval),color='black',size=4 )+
-  # theme(legend.position = "none")+
-  theme(panel.spacing = unit(1, "lines"))+
-  theme(legend.position = c(.65, .15))+
-  scale_color_manual(values=c('Non-infected'="#619CFF",'Infected'="#F8766D") ,labels=Lvalues,name='Litter Source')
-
-
-
